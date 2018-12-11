2bb76556c820288d7044a9f9b6f44c2245e9707d
andrewb9 <andrewb9@vt.edu>
2018-12-11 15:09:06 -0800

    Burger is now dynamically built with button controls

--- src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import Auxx from '../../../hoc/Auxx';
 import Burger from '../../Burger/Burger';
+import BuildControls from '../../Burger/BuildControls/BuildControls.js'
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 1,
+    meat: 2,
+    bacon: 1.5
+}
 
 class BurgerBuilder extends Component {
 
@@ -10,14 +18,47 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 3
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {...this.state.ingredients};
+        updatedIngredients[type] = updatedCount;
+        const updatedPrice = INGREDIENT_PRICES[type] + this.state.totalPrice;
+
+        this.setState({ingredients: updatedIngredients,totalPrice: updatedPrice}); 
+    }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+            if (oldCount <= 0) {
+              return;
+            }
+            const updatedCount = oldCount - 1;
+            const updatedIngredients = {...this.state.ingredients}
+            updatedIngredients[type] = updatedCount;
+            const updatedPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+            this.setState({ingredients: updatedIngredients, totalPrice: updatedPrice});
     }
 
     render () {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
         return (
             <Auxx>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls
+                ingredientAdded={this.addIngredientHandler}
+                ingredientRemoved={this.removeIngredientHandler}
+                disabled={disabledInfo}/>
             </Auxx>
         );
     }
